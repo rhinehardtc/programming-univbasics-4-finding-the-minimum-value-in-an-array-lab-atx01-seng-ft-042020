d2b322216380387d07aedcc7d65eeb83e4097fa1
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-02 23:41:26 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -2,10 +2,10 @@ def find_min_value(array)
   count = 0
   min_value = -1
   while count < array.length do
-    if min_value > array[count]
+    if min_value < array[count]
       count += 1
     end
-    if min_value < array[count]
+    if min_value > array[count]
       min_value = array[count]
     end
     count += 1
